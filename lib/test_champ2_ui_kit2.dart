6d77b37705bb768ab10faf7d56deaf4119153b82
Alex-rom1 <alex.rom495@gmail.com>
2026-01-07 11:48:03 +0300

    added textstyles and palettes to theme

--- lib/test_champ2_ui_kit2.dart
+++ lib/test_champ2_ui_kit2.dart
@@ -1,5 +1,2 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/presentation/theme/palettes.dart';
+export 'src/presentation/theme/texts.dart';
